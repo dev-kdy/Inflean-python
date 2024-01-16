79aca4432e35c6bd586816993470539efb0766dd
dev-kdy <404notfound.kdy@gmail.com>
2024-01-16 14:42:33 +0900

    파이참 - git 첫 연동 테스트 현재 날짜 출력

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# 샘플 Python 스크립트입니다.
+from datetime import datetime
+import pytz
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+# 아시아/서울 시간대
+seoul_timezone = pytz.timezone('Asia/Seoul')
 
+# 현재 날짜와 시간 가져오기
+current_time = datetime.now(seoul_timezone)
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
-
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('인프란 파이썬 학습')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+# 출력
+print("현재 날짜와 시간 (아시아/서울):", current_time)
