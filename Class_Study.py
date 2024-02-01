c40ede9af9ce4c1bc8cf7cd75c2f5605bdc8db01
dev-kdy <404notfound.kdy@gmail.com>
2024-02-01 15:32:05 +0900

    학습내용: Class, Static, Instance Method의 개념과 사용방법에 대해서 학습했다.

--- Class_Study.py
+++ Class_Study.py
@@ -1,40 +1,109 @@
+# Chapter02-02
+# 파이썬 심화
+# 객체 지향 프로그래밍(OOP) -> 코드의 재사용, 코드 중복 방지 등
+# 클래스 상세 설명
+# 클래스 변수, 인스턴스 변수
+
+# 클래스 재 선언
 class Car():
     """
-    Car class
+    Car Class
     Author : KDY
-    Date : 2024-01-16
+    Date : 2024.02.01
     """
+
+    # 클래스 변수
+    car_count = 0
 
     def __init__(self, company, details):
         self._company = company
         self._details = details
+        Car.car_count += 1
 
     def __str__(self):
-        return f'브랜드명 : {self._company}\n차 상세 정보 : {self._details}'
+        return 'str : {} - {}'.format(self._company, self._details)
 
     def __repr__(self):
-        return (
-            f'field:\n'
-            f'_company: {self._company}\n'
-            f'_details: {self._details}\n'
-        )
+        return 'repr : {} - {}'.format(self._company, self._details)
 
     def detail_info(self):
-        print("Id : {}\nprice_info : {}".format(id(self), self._details.get('price')))
+        print('Current Id : {}'.format(id(self)))
+        print('Car Detail Info : {} {}'.format(self._company, self._details.get('price')))
+
+    def __del__(self):
+        Car.car_count -= 1
 
 
-car1 = Car('Ferrari', {'color': 'White', 'horsepower': 400, 'price': 8000})
-car2 = Car('Bmw', {'color': 'Black', 'horsepower': 270, 'price': 5000})
-car3 = Car('Audi', {'color': 'Silver', 'horsepower': 300, 'price': 6000})
+# Self 의미
+car1 = Car('Ferrari', {'color' : 'White', 'horsepower': 400, 'price': 8000})
+car2 = Car('Bmw', {'color' : 'Black', 'horsepower': 270, 'price': 5000})
+car3 = Car('Audi', {'color' : 'Silver', 'horsepower': 300, 'price': 6000})
 
+# ID 확인
 print(id(car1))
 print(id(car2))
 print(id(car3))
 
+print(car1._company == car2._company)
 print(car1 is car2)
 
+# dir & __dict__ 확인
+print(dir(car1))
+print(dir(car2))
+
+print()
+print()
+
+print(car1.__dict__)
+print(car2.__dict__)
+
+# Doctring
+print(Car.__doc__)
+print()
+
+# 실행
+car1.detail_info()
 car2.detail_info()
 
-# Magic method __doc__을 출력 시 클래스 설명 주석이 나온다.
-print(car1.__doc__)
+# 에러
+# Car.detail_info()
+Car.detail_info(car1)
+Car.detail_info(car2)
 
+# 비교
+print(car1.__class__, car2.__class__)
+print(id(car1.__class__) == id(car3.__class__))
+
+print()
+
+# 인스턴스 변수
+# 직접 접근(PEP 문법적으로 권장X)
+print(car1._company, car2._company)
+print(car2._company, car3._company)
+
+print()
+print()
+
+# 클래스 변수
+
+# 접근
+print(car1.car_count)
+print(car2.car_count)
+print(Car.car_count)
+
+print()
+print()
+
+
+# 공유 확인
+print(Car.__dict__)
+print(car1.__dict__)
+print(car2.__dict__)
+print(car3.__dict__)
+
+# 인스턴스 네임스페이스 없으면 상위에서 검색
+# 즉, 동일한 이름으로 변수 생성 가능(인스턴스 검색 후 -> 상위(클래스 변수, 부모 클래스 변수))
+del car2
+
+print(car1.car_count)
+print(Car.car_count)
